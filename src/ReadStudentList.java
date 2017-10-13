a38652756da0134fc2e217e4719372c7e48daad4
Xingyue Sha <xingyue.sha@students.stamford.edu>
2017-10-13 10:52:05 +0700

    Update class diagram

--- src/ReadStudentList.java
+++
@@ -1,8 +0,0 @@
-import java.io.File;
-import java.io.IOException;
-
-public class ReadStudentList {
-    public void Read() throws IOException{
-        File file = new File("src/other/Studentlist-ITE222project.csv");
-    }
-}
